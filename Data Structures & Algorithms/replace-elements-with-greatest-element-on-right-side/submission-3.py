58129b73a9c6548c6f34b98c00ec926603887c55
Pavithra K <pavithra.kk29@gmail.com>
2026-04-03 13:00:51 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-3

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        max_right = -1
+        for i in range(len(arr)-1,-1,-1):
+            cur = arr[i]
+            arr[i] = max_right
+            max_right = max(max_right,cur)
+        return arr
+        
+
+        
