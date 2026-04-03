4d249cb30152c13fbe7a93a2b00c17a7eee11e5c
Pavithra K <pavithra.kk29@gmail.com>
2026-04-03 12:59:00 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        freq = Counter(nums)
+        for num,count in freq.items():
+            if count > 1:
+                return True
+        return False
+        
+
+        
